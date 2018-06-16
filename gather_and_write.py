05c887051666ff415b1c680ab56957d84d23944a
threexc <tvgamblin@gmail.com>
2018-06-15 21:38:44 -0400

    Added proper import; cleaned up indentation issues

--- gather_and_write.py
+++ gather_and_write.py
@@ -6,6 +6,7 @@ import datetime
 import os
 import errno
 import xml.etree.ElementTree as ET
+from WeatherData import *
 
 
 # Iteratively collect each aerodrome acronym from Aerodromes.xml, then generate
@@ -25,8 +26,10 @@ def gather_and_write(dromes_list="Aerodromes.xml"):
     for child in root:
 	# Get the aerodrome's abbreviation
         dromeID = child[0].text
-	weather_collector = WeatherCollector(dromeID)
+        weather_collector = WeatherCollector(dromeID)
         weather_collector.gather_data()
+        print(weather_collector.get_dromeID())
+
         weather_writer = WeatherWriter(dromeID, "../weather", weather_collector)
         weather_writer.write_metar(weather_collector.get_metar())
         weather_writer.write_taf(weather_collector.get_taf())
