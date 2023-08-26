5cd0efa89dce2276e661e7298f5232fd72dea542
Trevor Gamblin <tvgamblin@gmail.com>
2023-08-26 09:21:07 -0400

    weathervane: improve bulletin fetching
    - Rename forecast.py to bulletin.py
    - Rename the forecast() function to _get()
    - Add forecast() and observations() functions
    
    Signed-off-by: Trevor Gamblin <tvgamblin@gmail.com>

--- src/weathervane/bulletin.py
+++ src/weathervane/bulletin.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 # NAV CANADA api-endpoint
 URL = "https://flightplanning.navcanada.ca/cgi-bin/Fore-obs/metar.cgi"
 
-def forecast(aerodrome, filename=None):
+def _get(aerodrome, filename=None):
     PARAMS = {
         'NoSession': "",
         'Stations': aerodrome,
@@ -32,3 +32,18 @@ def forecast(aerodrome, filename=None):
             content = f.read()
 
     return content
+
+def forecast(aerodrome, filename=None):
+    return _get(aerodrome, filename).split(" TAF ")[1]
+
+# Observations are more complicated to extract than forecasts. We need
+# to take the first split of the _get result, split on occurrences of
+# "METAR", and filter out the first two elements in the list (which will
+# be indicators of aerodrome location). Then, since there are
+# potentially multiple METARs (as opposed to a single TAF), strip
+# whitespace from each and then join them into a single string with
+# newlines.
+def observations(aerodrome, filename=None):
+    result = _get(aerodrome, filename).split(" TAF ")[0].split("METAR")[2:]
+    return '\n'.join(line.strip() for line in result)
+
